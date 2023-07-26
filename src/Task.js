69bd9bb51f4c6611e6a0b11606d4c9ffbd1e9526
Siti Nur Kholifah <lilyy.sitink@gmail.com>
2023-07-26 14:06:08 +0700

    lili

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://fp-reactjs.web.app/" // TODO: replace this
+const studentName = 'Siti Nur Kholifah' // TODO: replace this
+const studentId = 'FE5174358' // TODO: replace this
+
+export { url, studentName, studentId }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+// TODO: answer here
+import { Box } from '@chakra-ui/react';
+
+const Footer = () => {
+  return (
+    // TODO: answer here
+    <Box className="footer">
+      <p className="studentName">Siti Nur Kholifah</p>
+      <p className="studentId">FE5174358</p>
+    </Box>
+  );
+};
+
+export default Footer;
